6c2219c9ca852e6d73ccd388963ea95007a5502b
clemente alvarez <realestate.portal2018@gmail.com>
2018-04-08 00:41:17 +0100

    esta eslamanera de mezclar html y php antes de todo en el head en el body en tipo entrada tipo text

---
+++ mezclar_html_php.php
@@ -0,0 +1,13 @@
+<?php print "hola"<br>; ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Mezclar html y php</title>
+    <?php print "hola desde el head"<br>; ?>s
+  </head>
+  <body>
+    <?php print "hola desde el body"<br>; ?>
+    <input type="text" name="" value="<?php print 'hola, cara de bola'?>">
+  </body>
+</html>
